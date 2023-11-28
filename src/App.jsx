2780764a105e279d66aaae01dd26975a05ac2bf6
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-11-28 21:54:41 +0100

    Challenge React Basics 04

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import PokemonCard from './components/PokemonCard/PokemonCard.jsx';
 
 function App() {
 
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const pokemon = pokemonList[0];
+
   return (
     <>
       <div>
         <MyTitle />
       </div>
       <div>
-        <PokemonCard />
+        <PokemonCard pokemon={pokemon}/>
       </div>
     </>
   );
--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,28 +1,21 @@
 import styles from './PokemonCard.module.css';
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-const PokemonCard = () => {
-
-    const pokemon = pokemonList[1];
+const PokemonCard = (props) => {
+console.log(props.pokemon);
 
     return (
         <figure className={styles.card}>
-            {pokemon.imgSrc ? (
-                <img src={pokemon.imgSrc} alt={pokemon.name} className={styles.cardImg} />
+            {props.pokemon.imgSrc ? (
+                <img src={props.pokemon.imgSrc} alt={props.pokemon.name} className={styles.cardImg} />
                 ) : <p>???</p>}
-            <figcaption>{pokemon.name}</figcaption>
+            <figcaption>{props.pokemon.name}</figcaption>
         </figure>
     );
 };
 
 export default PokemonCard;
+
+// 5. Utiliser props.pokemon : dans le composant PokemonCard, utilise props.pokemon pour réparer l'affichage de ton composant (pense à la destructuration). Tu devrais maintenant être en mesure de voir le Pokémon correctement affiché.
+
+// 6. Validation : assure-toi que ton composant PokemonCard utilise props pour recevoir les données du Pokémon et que tout fonctionne correctement.
+
