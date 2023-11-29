cd77abedc40a84d8efc8fabfe7628583cf97594d
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-11-29 19:48:52 +0100

    Challenge done

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,7 +1,6 @@
 import styles from './PokemonCard.module.css';
 
 const PokemonCard = (props) => {
-console.log(props.pokemon);
 
     return (
         <figure className={styles.card}>
@@ -14,8 +13,4 @@ console.log(props.pokemon);
 };
 
 export default PokemonCard;
-
-// 5. Utiliser props.pokemon : dans le composant PokemonCard, utilise props.pokemon pour réparer l'affichage de ton composant (pense à la destructuration). Tu devrais maintenant être en mesure de voir le Pokémon correctement affiché.
-
-// 6. Validation : assure-toi que ton composant PokemonCard utilise props pour recevoir les données du Pokémon et que tout fonctionne correctement.
 
