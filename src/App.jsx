b493d9a34506aa3564bb217324854927fbb35c67
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-11-30 21:27:31 +0100

    Ajout commentaires détaillés

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import './App.css';
+// Import des composants
 import MyTitle from './components/MyTitle/MyTitle.jsx';
 import PokemonCard from './components/PokemonCard/PokemonCard.jsx';
+
+// Import du hook useState qui va définir l'état local
 import { useState } from "react";
 
 function App() {
 
+  // Déclaration de l'état local avec le getter pokemonIndex et le setter setPokemonIndex initialisé à 0
   const [pokemonIndex, setPokemonIndex] = useState(0);
-  console.log(pokemonIndex);
 
-
+  // Déclaration de la fonction handleClick qui prend en paramètre la direction
   const handleClick = (direction) => {
+    // Si la direction est "previous" et que l'index est supérieur à 0, on décrémente l'index
     if (direction === "previous" && pokemonIndex > 0) {
       setPokemonIndex(pokemonIndex - 1);
+
+    // Sinon si la direction est "next" et que l'index est inférieur à la longueur du tableau - 1, on incrémente l'index
     } else if (direction === "next" && pokemonIndex < pokemonList.length - 1) {
       setPokemonIndex(pokemonIndex + 1);
     }
   };
 
+  // Déclaration du tableau pokemonList
   const pokemonList = [
     {
         name: "bulbasaur",
@@ -43,21 +50,30 @@ function App() {
       },
     ];
 
+    // Déclaration de la constante currentPokemon qui récupère l'objet pokemonList à l'index pokemonIndex
     const currentPokemon = pokemonList[pokemonIndex];
 
+  // Affichage du composant App
   return (
     <>
       <div>
         <MyTitle />
       </div>
       <div>
+
+        {/* Affichage du composant PokemonCard avec la props pokemon qui prend la valeur de la constante currentPokemon */ }
         <PokemonCard pokemon={currentPokemon}/>
       </div>
+
+      {/* Si poskemonIndex est supérieur à 0 on affiche le bouton "précédent'. On lance la fonction handleClick en passant en paramètre "previous*/ }
       {pokemonIndex > 0 && <button onClick={() => handleClick("previous")}>Précédent</button>}
+
+      {/* Si poskemonIndex est inférieur à la longueur du tableau - 1 on affiche le bouton "suivant'. On lance la fonction handleClick en passant en paramètre "next*/ }
       {pokemonIndex < pokemonList.length - 1 && <button onClick={() => handleClick("next")}>Suivant</button>}
     </>
   );
 };
 
+// Export du composant App
 export default App;
 
--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,23 +1,35 @@
 import styles from './PokemonCard.module.css';
+
+// On importe la librairie prop-types qui sert à typer les props
 import PropTypes from "prop-types";
 
+// On déclare le composant PokemonCard en lui passant la props pokemon
 const PokemonCard = (props) => {
 
+    // On type les props du composant PokemonCard
     PokemonCard.propTypes = {
+        // qui sont de type objet
         pokemon: PropTypes.shape ({
+        // qui contiennent une propriété name de type string et qui est obligatoire
         name: PropTypes.string.isRequired,
+        // qui contiennent une propriété imgSrc de type string facultative
         imgSrc: PropTypes.string
       }),
     }
 
+    // Affichage du composant PokemonCard
     return (
+        // La balise <figure> est une balise HTML utilisée pour encapsuler tout type de contenu qui référence une figure, comme une image, un schéma, une illustration, un code, etc.
         <figure className={styles.card}>
+            {/* Si props.pokemon.imgSrc existe (n'est pas undefined, null, ou une chaîne vide), alors une balise <img> est rendue avec l'attribut src défini par props.pokemon.imgSrc. Sinon on affiche '???' */ }
             {props.pokemon.imgSrc ? (
                 <img src={props.pokemon.imgSrc} alt={props.pokemon.name} className={styles.cardImg} />
                 ) : <p>???</p>}
+            {/* <figcaption> : Il s'agit d'une balise HTML utilisée généralement à l'intérieur d'une balise <figure>. Elle est utilisée pour décrire ou légender le contenu de la balise <figure> */}
             <figcaption>{props.pokemon.name}</figcaption>
         </figure>
     );
 };
 
+// On exporte le composant PokemonCard
 export default PokemonCard;
