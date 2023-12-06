7c0b0082ce4c905ccb69e68be550f3357a68e6f9
Nina IACOPONELLI <nina.iacoponelli@gmail.com>
2023-12-06 22:02:33 +0100

    En cours React Basics 08bis

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import NavBar from './components/Navbar/Navbar.jsx';
 // Import de la librairie React
 import React from "react";
 
-// Import du hook useState qui va définir l'état local
+// Import des hooks : useState qui va définir l'état local et useEffect qui va permettre d'effectuer des effets de bord sans action de la part de l'utilisateur
 import { useState } from "react";
+import { useEffect } from "react";
 
 // Utilisation de la classe ErrorBoundary pour gérer les erreurs
 class ErrorBoundary extends React.Component {
@@ -36,6 +37,13 @@ class ErrorBoundary extends React.Component {
 }
 
 function App() {
+
+  useEffect(
+    () => {
+      console.log('hello pokemon trainer :)');
+    },
+    []
+  );
 
   // Déclaration de l'état local avec le getter pokemonIndex et le setter setPokemonIndex initialisé à 0
   const [pokemonIndex, setPokemonIndex] = useState(0);
@@ -71,7 +79,7 @@ function App() {
   const updatePokemon = (index) => {
     setPokemonIndex(index);
   };
-  
+
   // Déclaration de la constante currentPokemon qui récupère l'objet pokemonList à l'index pokemonIndex
   const currentPokemon = pokemonList[pokemonIndex];
 
@@ -95,3 +103,11 @@ function App() {
 
 // Export du composant App
 export default App;
+
+// 2. Ouvrir une alerte au démarrage : dans le composant App, ouvre une alerte au "démarrage" de l'application avec le message "hello pokemon trainer :)". Tu peux utiliser le hook useEffect ici.
+
+// 3. Ouvrir une alerte lors de la sélection de Pikachu : dans le composant NavBar, détecte lorsque le Pokémon sélectionné devient "pikachu". Lorsque cela se produit, ouvre une alerte avec le message "pika pikachu !!!". Tu ne devrais pas utiliser le hook useEffect ici (voir "You Might Not Need an Effect").
+
+// 4. Validation : vérifie que les alertes s'affichent correctement au démarrage de l'application et lorsque Pikachu est sélectionné dans NavBar.
+
+// 5. Soumission : fournis le lien vers la branche events.1 de ton dépôt GitHub pour valider cette étape.
